ceab30e39245d3fd9b4a71a16bb22dd1967cbbd2
Dawei Chen <dche0119@student.monash.edu>
2025-08-07 18:27:09 +0800

    feat(core)： Search teacher_db by name or speciality(case-intensitive).

--- MSMS.py
+++ MSMS.py
@@ -1,4 +1,4 @@
-# MSMS.py - The In-Memory Prototype
+# MSMS.py - The In-Memory Prototype.
 
 # --- Data Models ---
 class Student:
@@ -7,7 +7,7 @@ class Student:
         self.id = student_id
         self.name = name
         # TODO: Initialize an empty list called 'enrolled_in' to store instrument names.
-        self.enrolled_in = []
+        self.enroll_in = []
 
 class Teacher:
     """A blueprint for teacher objects."""
@@ -17,13 +17,25 @@ class Teacher:
         self.id = teacher_id
         self.name = name
         self.speciality = speciality
+        self.teaching = []
+
+class Course:
+    def __init__(self, course_code, course_name):
+        self.code = course_code
+        self.name = course_name
+        self.teacher = []
+        self.student = []
+
+# TODO: Assign all three parameters(course code, name)
 
 # --- In-Memory Databases ---
 # TODO: Create the global data stores.
 student_db = []
 teacher_db = []
+course_db = []
 next_student_id = 1
 next_teacher_id = 1
+next_course_code = 1
 
 # --- Core Helper Functions ---
 
@@ -49,6 +61,14 @@ def add_teacher(name, speciality):
     next_teacher_id += 1
     print(f"Core: Teacher '{name}' added successfully.")
 
+def create_course(course_code, course_name):
+    """Creates a course object and adds it to the database."""
+    global next_course_code
+    new_course = Course(course_code,course_name)
+    course_db.append(new_course)
+    next_course_code += 1
+    print(f"Core: Course '{course_name}' added successfully.")
+
 def list_students():
     """Prints all students in the database."""
     print("\n--- Student List ---")
@@ -66,15 +86,20 @@ def list_teachers():
     for teacher in teacher_db:
         print(f"  ID: {teacher.id}, Name: {teacher.name}, Speciality: {teacher.speciality}")
 
-def find_students(term):
+def list_course():
+    print("\n--- Teacher List ---")
+    for course in course_db:
+        print(f"  Code: {course.code}, Subject: {course.name}, Lecturer: {course.teacher}")
+
+def find_students(info):
     """Finds students by name."""
-    print(f"\n--- Finding Students matching '{term}' ---")
+    print(f"\n--- Finding Students matching '{info}' ---")
     # TODO: Create an empty list to store results.
     student_list = []
     # Loop through student_db. If the search 'term' (case-insensitive) is in the student's name,
     # add them to your results list.
     for student in student_db:
-        if student.name == term:
+        if student.name == info:
             student_list.append(student)
     # After the loop, if the results list is empty, print "No match found."
     if not student_list:
@@ -89,7 +114,7 @@ def find_teachers(info):
     # TODO: Implement this function similar to find_students, but check
     teacher_list = []
     for teacher in teacher_db:
-        if teacher.name == info or teacher.speciality == info:
+        if teacher.name.upper() == info.upper() or teacher.speciality.upper() == info.upper():
             teacher_list.append(teacher)
     if not teacher_list:
         print("No match found.")
@@ -100,7 +125,7 @@ def find_teachers(info):
         print("No match found.")
     else:
         for teacher in teacher_list:
-            print(f" ID: {teacher.id}, Name: {teacher.name}")
+            print(f" ID: {teacher.id}, Name: {teacher.name}, speciality: {teacher.speciality}")
     # for the term in BOTH the teacher's name AND their speciality.
 
 
